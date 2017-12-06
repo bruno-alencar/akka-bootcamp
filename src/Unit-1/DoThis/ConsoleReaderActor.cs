bcd16a7d6e0ae7a17d98f673d54082eae17d9918
Bruno Alencar <bruno.idealencar@gmail.com>
2017-12-06 12:58:50 -0200

    Exercise 5 done

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -11,12 +11,6 @@ namespace WinTail
     {
         public const string ExitCommand = "exit";
         public const string StartCommand = "start";
-        private readonly IActorRef _validationActor;
-
-        public ConsoleReaderActor(IActorRef validationActor)
-        {
-            _validationActor = validationActor;
-        }
 
         protected override void OnReceive(object message)
         {
@@ -52,7 +46,8 @@ namespace WinTail
 
             // otherwise, just hand message off to validation actor
             // (by telling its actor ref)
-            _validationActor.Tell(message);
+            //_validationActor.Tell(message);
+            Context.ActorSelection("akka://MyActorSystem/user/validationActor").Tell(message);
         }
         #endregion
     }
